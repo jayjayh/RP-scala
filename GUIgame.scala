bdc93b678eb8a8bc6374495ba3dd1e88600fa26b
Jason Chen <jasonchen877@gmail.com>
2020-12-11 23:22:31 -0500

    create grid and triangle

--- GUIgame.scala
+++ GUIgame.scala
@@ -1,24 +1,21 @@
+import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
-import scalafx.print.PrintColor.Color
 import scalafx.scene.Scene
-import scalafx.scene.control.{Alert, Button}
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
+import scalafx.scene.control.Button
+import scalafx.scene.input.{KeyCode, KeyEvent}
 import scalafx.scene.paint.Color._
 import scalafx.scene.paint.{LinearGradient, Stops}
-import scalafx.scene.shape.Rectangle
+import scalafx.scene.shape.{Line, Polygon, Rectangle}
 import scalafx.scene.text.Text
-import scalafx.Includes._
-import scalafx.scene.control.Alert.AlertType
-import scalafx.scene.input.{KeyCode, KeyEvent}
 
 object GUIgame extends JFXApp {
+  val cellSize = 50
   stage = new PrimaryStage {
     title = "GUI Game"
-    val swidth = 600
-    val sheight = 600
+    val swidth = 500
+    val sheight = 500
+
     scene = new Scene(swidth,sheight) {
       fill = White
       val button = new Button("Restart")
@@ -29,6 +26,7 @@ object GUIgame extends JFXApp {
           endX = 0,
           stops = Stops(PaleGreen, SeaGreen))
       }
+
       onKeyPressed = (ev:KeyEvent) => {
         if( ev.code == KeyCode.Left && player.getX-50 >= 0 ) {
           player.x = player.getX - 50
@@ -43,16 +41,39 @@ object GUIgame extends JFXApp {
           player.y = player.getY + 50
         }
       }
+
+      val grid: Seq[Line] = createGrid(swidth,sheight)
+
       val player: Rectangle = new Rectangle{
         x = 300
         y = 300
-        width = 25
-        height = 25
+        width = cellSize
+        height = cellSize
         fill = Green
       }
-      content = Seq(
-        player
+
+      content = grid ++ Seq(
+        player,triangle(4,4,"RtL")
       )
     }
+  }
+  def createGrid(height:Int,width:Int):Seq[Line] = {
+    var xgrid: Seq[Line] = Seq()
+    var ygrid: Seq[Line] = Seq()
+    xgrid = for (n <- (0 to height).by(cellSize)) yield new Line {startX = 0; startY = n; endX = width; endY = n}
+    ygrid = for (n <- (0 to width).by(cellSize)) yield new Line {startX = n; startY = 0; endX = n; endY = height}
+    xgrid++ygrid
+  }
+  def triangle(x:Double, y:Double,dir:String) = {
+    val triangle = new Polygon()
+    dir match {
+      case "LtR" => triangle.getPoints.setAll(0+(x*cellSize), 0+(y*cellSize), 0+(x*cellSize), cellSize+(y*cellSize), cellSize+(x*cellSize), (cellSize/2)+(y*cellSize))
+      case "RtL" => triangle.getPoints.setAll(cellSize+(x*cellSize), 0+(y*cellSize), cellSize+(x*cellSize), cellSize+(y*cellSize), 0+(x*cellSize), (cellSize/2)+(y*cellSize))
+      case "TtB" => triangle.getPoints.setAll(0+(x*cellSize), 0+(y*cellSize), (cellSize/2)+(x*cellSize), cellSize+(y*cellSize), cellSize+(x*cellSize), 0+(y*cellSize))
+      case "BtT" => triangle.getPoints.setAll(0+(x*cellSize), cellSize+(y*cellSize), (cellSize/2)+(x*cellSize), 0+(y*cellSize), cellSize+(x*cellSize), cellSize+(y*cellSize))
+      case _ => triangle.getPoints.setAll(0+(x*cellSize), 0+(y*cellSize), 0+(x*cellSize), cellSize+(y*cellSize), cellSize+(x*cellSize), (cellSize/2)+(y*cellSize))
+    }
+    triangle.fill= Orange
+    triangle
   }
 }
