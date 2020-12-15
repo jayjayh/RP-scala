210aff03ad9c284e82f00e181d5d3dbb85a6dac5
Jesse Huang <jesse.huang1205@gmail.com>
2020-12-15 13:49:55 -0500

    Add files via upload

--- GUIgame.scala
+++ GUIgame.scala
@@ -2,6 +2,7 @@ import scalafx.Includes._
 import scalafx.animation.PauseTransition
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+//import scalafx.scene.input.{KeyCode, KeyEvent}
 import scalafx.scene.text.Text
 //import scalafx.event.ActionEvent.Any
 import scalafx.event.{ActionEvent, Event}
@@ -28,12 +29,12 @@ object GUIgame extends JFXApp {
     var gameover:Boolean = false
     var tris: Seq[Polygon] = Seq()
     val grid: Seq[Line] = createGrid(swidth,sheight)
-//    val player: Circle = new Circle{
-//      centerX= swidth/2
-//      centerY= sheight/2
-//      radius= cellSize/2
-//      fill = Green
-//    }
+    //    val player: Circle = new Circle{
+    //      centerX= swidth/2
+    //      centerY= sheight/2
+    //      radius= cellSize/2
+    //      fill = Green
+    //    }
     val quitbutton:Button = new Button {
       text = "Quit"
       onAction = () => {
@@ -41,9 +42,8 @@ object GUIgame extends JFXApp {
       }
     }
     val timer = new PauseTransition(Duration(200))
-    timer.play
     //    createTri(0,0,"TtB",rand.nextInt(3)+1,draw)
-    val draw= (n:Polygon)=> {
+    val draw: Polygon => Unit = (n:Polygon)=> {
       tris = tris:+n
       n.fireEvent(new ActionEvent())
     }
@@ -53,12 +53,16 @@ object GUIgame extends JFXApp {
       val startButton:Button = new Button {
         text = "Start Game"
         onAction = (ev: Event) => {
-          println(ev)
+          //println(ev)
           if (!triggered) {
+            println("Game Started")
             genTri()
-            triggered = true
             gameover = false
-//            println("Mouse enter, genTri()")
+            if(!triggered)
+              addPoint(score,0)
+            triggered = true
+            timer.play
+            //            println("Mouse enter, genTri()")
           }
         }
       }
@@ -67,18 +71,21 @@ object GUIgame extends JFXApp {
         translateX=5
         onAction = () => {
           points=0
+          addPoint(score,0)
           gameover = true
           triggered = false
           println("Restarted")
+          if(tris.nonEmpty)
+            tris.foreach(delete)
+          content = grid ++ Seq(hbox) ++ tris
         }
       }
-      var points=0;
-      val addPoint= (tb:Text,p:Int)=>{
+      var points=0
+      val addPoint: (Text, Int) => Unit = (tb:Text, p:Int)=>{
         if(!gameover) {
           points+=p
           tb.text="Score: " +points.toString
         }
-
       }
       val score: Text = new Text{
         //translateX = 50
@@ -88,26 +95,27 @@ object GUIgame extends JFXApp {
 
         layoutY = 555
 
-//        val time: Text = new Text{
-//                  translateX = 200
-//                  text = "Time: " + "temp time"
-//                }
+        //        val time: Text = new Text{
+        //                  translateX = 200
+        //                  text = "Time: " + "temp time"
+        //                }
         quitbutton.translateX = 330
         startButton.translateX = 175
         children = Seq(restartbutton,startButton,score/*,time*/,quitbutton)
       }
-//      onKeyPressed = (ev:KeyEvent) => {
-//        ev.code match{
-//          case KeyCode.Left =>  println(tris)
-//          case KeyCode.Right => println(gameover)
-//          case KeyCode.Up => player.layoutY = player.getLayoutY - 50
-//          case KeyCode.Down=> player.layoutY = player.getLayoutY + 50
-//        }
-//      }
+//            onKeyPressed = (ev:KeyEvent) => {
+//              ev.code match{
+//                case KeyCode.Left =>  println(tris)
+//                case KeyCode.Right => println(gameover)
+//                case KeyCode.Down => println(points)
+//                case _ =>
+//              }
+//            }
 
-      val update=()=>{
+      val update: () => Unit = ()=>{
         if(!gameover){
           content = grid ++ Seq(hbox) ++ tris
+          //println("updated")
           timer.playFromStart()
         }
       }
@@ -127,12 +135,11 @@ object GUIgame extends JFXApp {
             Thread.sleep(1000)
           }
         }.onComplete{
-          case Success(s) => {
+          case Success(s) =>
             if(!gameover) {
               genTri()
               genRandTriangle(addPoint(score,_))
             }
-          }
           case Failure(f) => println(f)
         }
       }
@@ -159,7 +166,7 @@ object GUIgame extends JFXApp {
         }
         xy
       }
-      def genRandTriangle(cb:(Int)=>Unit): Unit ={
+      def genRandTriangle(cb: Int =>Unit): Unit ={
         Future {
           var tri: Polygon = null
           val rand = new Random()
@@ -175,7 +182,7 @@ object GUIgame extends JFXApp {
           val movex:Double = v.head
           val movey = v(1)
           val speed = v(2)
-          while(!gameover && (0) <= swidth+(cellSize) && xxx(0) >= 0 && xxx(1) <= sheight+(cellSize) && xxx(1) >= 0) {
+          while(!gameover && 0 <= swidth+cellSize && xxx(0) >= 0 && xxx(1) <= sheight+ cellSize && xxx(1) >= 0) {
             tri.getPoints.setAll(xxx(0) + movex,xxx(1) + movey,xxx.get(2)+ movex,xxx(3) + movey,xxx(4)+movex,xxx(5) + movey)
             xxx = tri.getPoints
             Thread.sleep(speed.toInt)
@@ -203,7 +210,7 @@ object GUIgame extends JFXApp {
     def triangle(x:Double, y:Double,dir:String,steps:Int,d:Polygon => Unit): Polygon = {
       val triangle = new Polygon()
       triangle.userData = dir
-      triangle.onMouseEntered() = {() => println("Triangle touched");triggered = false;gameover = true}
+      triangle.onMouseEntered() = {() => /*println("Triangle touched");*/gameover = true}
       dir match {
         case "LtR" => triangle.getPoints.setAll(1+(x*cellSize), 1+(y*cellSize), 1+(x*cellSize), cellSize-1+(y*cellSize), cellSize-1+(x*cellSize), (cellSize/2)-.5+(y*cellSize))
         case "RtL" => triangle.getPoints.setAll(cellSize-1+(x*cellSize), 1+(y*cellSize), cellSize-1+(x*cellSize), cellSize-1+(y*cellSize), 1+(x*cellSize), (cellSize/2)-.5+(y*cellSize))
@@ -222,3 +229,136 @@ object GUIgame extends JFXApp {
     }
   }
 }
+
+/*
+
+object GUIgame extends JFXApp {
+  val cellSize = 50
+  //  val t = new java.util.Timer()
+  //  val task = new java.util.TimerTask {
+  //    def run() = println("Beep!")
+  //  }
+  //  t.schedule(task, 1000L, 1000L)
+  //  task.cancel()
+  stage = new PrimaryStage{
+    title = "GUI Game"
+    val timer = new PauseTransition(Duration(1500))
+    timer.play
+    height = 625
+    val swidth = 550
+    val sheight = 550
+    var tris: Seq[Polygon] = Seq()
+    val grid: Seq[Line] = createGrid(swidth,sheight)
+    val player: Circle = new Circle{
+      centerX= swidth/2
+      centerY= sheight/2
+      radius= cellSize/2
+      fill = Green
+    }
+    val quitbutton:Button = new Button {
+      text = "Quit"
+      relocate(500,555)
+      onAction = () => {
+        stage.close()
+      }
+    }
+    createTri(0,0,"TtB",draw)
+    def draw(n:Polygon): Unit = {
+      tris = tris:+n
+    }
+    def printXY(tri:Polygon)={
+      println(tri.getPoints)
+    }
+    scene = new Scene(swidth,sheight) {
+      fill = White
+      genTri()
+//      onKeyPressed = (ev:KeyEvent) => {
+//        if( ev.code == KeyCode.Left) {
+//          println("left")
+//          println(tris)
+//        }
+//        else if( ev.code == KeyCode.Right) {
+//          println("right")
+//        }
+//        else if( ev.code == KeyCode.Up) {
+//          println("up")
+//        }
+//        else if(ev.code == KeyCode.Down){
+//          println("down")
+//        }
+//      }
+      val restartbutton:Button = new Button {
+        text = "Restart"
+        relocate(10,555)
+        onAction = () => {
+          println("button pressed")
+          tris.head.layoutX = tris.head.getLayoutX + 50
+        }
+      }
+      def update():Unit ={
+        content = grid ++ Seq(player,restartbutton,quitbutton) ++ tris
+        timer.playFromStart()
+      }
+      timer.onFinished = {() =>update();println("updated")}
+      def genTri(): Unit ={
+        val r = new Random()
+        Future{
+          Thread.sleep(1000)
+          while(r.nextInt(6) != 0){
+            Thread.sleep(1000)
+          }
+        }.onComplete{
+          case Success(s) => {
+            genRandTriangle()
+            genTri()
+          }
+          case Failure(f) => println(f)
+        }
+      }
+      def genRandTriangle(): Unit ={
+
+        val rand = new Random()
+        rand.nextInt(4) match{
+          case 0 => createTri(0,rand.nextInt(11),"LtR",draw); println("generated triangle")
+          case 1 => createTri(rand.nextInt(11),0,"TtB",draw); println("generated triangle")
+          case 2 => createTri(10,rand.nextInt(11),"RtL",draw); println("generated triangle")
+          case 3 => createTri(rand.nextInt(11),10,"BtT",draw); println("generated triangle")
+          case _ => println("Error")
+        }
+      }
+      content = grid ++ Seq(player,restartbutton,quitbutton) ++ tris
+
+    }
+  }
+
+  def createTri(x:Double,y:Double,dir:String,d:Polygon => Unit): Unit = {
+    val tri= Future{
+      triangle(x,y,dir)
+    }
+    tri.onComplete {
+      case Failure(ex) => println(ex.getMessage)
+      case Success(v) => d(v)
+    }
+  }
+  def createGrid(height:Int,width:Int):Seq[Line] = {
+    var xgrid: Seq[Line] = Seq()
+    var ygrid: Seq[Line] = Seq()
+    xgrid = for (n <- (0 to height).by(cellSize)) yield new Line {startX = 0; startY = n; endX = width; endY = n}
+    ygrid = for (n <- (0 to width).by(cellSize)) yield new Line {startX = n; startY = 0; endX = n; endY = height}
+    xgrid++ygrid
+  }
+  def triangle(x:Double, y:Double,dir:String): Polygon = {
+    val triangle = new Polygon()
+    dir match {
+      case "LtR" => triangle.getPoints.setAll(0+(x*cellSize), 0+(y*cellSize), 0+(x*cellSize), cellSize+(y*cellSize), cellSize+(x*cellSize), (cellSize/2)+(y*cellSize))
+      case "RtL" => triangle.getPoints.setAll(cellSize+(x*cellSize), 0+(y*cellSize), cellSize+(x*cellSize), cellSize+(y*cellSize), 0+(x*cellSize), (cellSize/2)+(y*cellSize))
+      case "TtB" => triangle.getPoints.setAll(0+(x*cellSize), 0+(y*cellSize), (cellSize/2)+(x*cellSize), cellSize+(y*cellSize), cellSize+(x*cellSize), 0+(y*cellSize))
+      case "BtT" => triangle.getPoints.setAll(0+(x*cellSize), cellSize+(y*cellSize), (cellSize/2)+(x*cellSize), 0+(y*cellSize), cellSize+(x*cellSize), cellSize+(y*cellSize))
+      case _ => triangle.getPoints.setAll(0+(x*cellSize), 0+(y*cellSize), 0+(x*cellSize), cellSize+(y*cellSize), cellSize+(x*cellSize), (cellSize/2)+(y*cellSize))
+    }
+    triangle.fill= Orange
+    triangle
+  }
+}
+
+*/
